e7f865337a2fa55c50c5e21ebacb8c3ba78bcc35
Tom Swartz <tom.swartz07@gmail.com>
2022-01-05 22:51:57 -0500

    Fixup path issue for noninteractive sessions

--- twitter.sh
+++ twitter.sh
@@ -6,5 +6,5 @@ DB_NAME="${PGDATABASE:-postgres}"
 DB_USER="${PGUSER:-postgres}"
 
 /python-fav-tweets.py
-psql -d "${DB_NAME}" -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --file=postgres-fav-tweets.sql
+psql -d "${DB_NAME}" -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --file=/postgres-fav-tweets.sql
 #vacuumdb -q -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --jobs=2 --analyze -w --table='twitter.favorite_tweets' "${DB_NAME}"
