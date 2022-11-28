59e26c1d5c95adc7805b2be342b33b36e2ec1540
Tom Swartz <tom.swartz07@gmail.com>
2022-11-28 00:29:15 -0500

    Test using connection string params

--- twitter.sh
+++ twitter.sh
@@ -6,5 +6,6 @@ DB_NAME="${PGDATABASE:-postgres}"
 DB_USER="${PGUSER:-postgres}"
 
 /python-fav-tweets.py
-psql -d "${DB_NAME}" -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --file=/postgres-fav-tweets.sql
+psql "host=${DB_HOST} port=${DB_PORT} user=${DB_USER} dbname=${DB_NAME} application_name=TwitterImporter" --file=/postgres-fav-tweets.sql
+#psql -d "${DB_NAME}" -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --file=/postgres-fav-tweets.sql
 #vacuumdb -q -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --jobs=2 --analyze -w --table='twitter.favorite_tweets' "${DB_NAME}"
