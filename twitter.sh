7f51494312fdeed493aeefadd9d9d9a4e8a62232
Tom Swartz <tom.swartz07@gmail.com>
2023-01-13 11:32:58 -0500

    Add date to output messages

--- twitter.sh
+++ twitter.sh
@@ -5,6 +5,7 @@ DB_PORT="${PGPORT:-5432}"
 DB_NAME="${PGDATABASE:-postgres}"
 DB_USER="${PGUSER:-postgres}"
 
+date
 /python-fav-tweets.py
 psql "host=${DB_HOST} port=${DB_PORT} user=${DB_USER} dbname=${DB_NAME} application_name=TwitterImporter" --file=/postgres-fav-tweets.sql
 #psql -d "${DB_NAME}" -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" --file=/postgres-fav-tweets.sql
